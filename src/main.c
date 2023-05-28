1eb6dcbef0be8a1e660fcd5385e73bf22f26701b
Jack Whitehorn <jackwh.whitehorn@gmail.com>
2023-05-28 19:28:21 +0100

    Start on checkers

--- src/main.c
+++ src/main.c
@@ -5,6 +5,57 @@
 #include "token.h"	
 #include "ast.h"
 
+bool ast_check_symbol_exists(ast_t* ast, str_t symbol, str_array_t* symbols) {
+	static const char* PRIMITIVES[] = { "u8", "u16", "u32", "u64", "usize", "i8", "i16", "i32", "i64", "isize", "f32", "f64" };
+	arena_t scratch = arena_new();
+	
+	for (usize i = 0; i < symbols->size; i++) {
+		if (str_eq(symbol, symbols->data[i])) return true;
+	}
+	for (usize i = 0; i < ast->types.size; i++) {
+		if (str_eq(symbol, ast->types.data[i].name)) return true;
+	}
+	for (usize i = 0; i < (sizeof(PRIMITIVES) / sizeof(*PRIMITIVES)); i++) {
+		if (str_eq(symbol, str_lit(&scratch, PRIMITIVES[i]))) return true;
+	}
+
+	ERROR("Cannot find type symbol: %s", str_cstr(&scratch, &symbol));
+	arena_destroy(&scratch);
+	return false;
+}
+
+
+bool ast_check_type(ast_t* ast, type_t* type, str_array_t* symbols);
+bool ast_check_type_value(ast_t* ast, type_value_t* type_value, str_array_t* symbols) {
+	bool out = ast_check_symbol_exists(ast, type_value->name, symbols);
+	for (usize i = 0; i < type_value->generics.size; i++) {
+		out &= ast_check_type(ast, &type_value->generics.data[i], symbols);
+	}
+	return out;
+}
+
+bool ast_check_type(ast_t* ast, type_t* type, str_array_t* symbols) {
+	if (type->type == TYPE_VALUE) {
+		return ast_check_type_value(ast, &type->data.value, symbols);
+	} else {
+		return true;
+	}
+}
+
+bool ast_check_type_decl(ast_t* ast, type_decl_t* type_decl) {
+	return ast_check_type(ast, &type_decl->type, &type_decl->generics);
+}
+
+bool ast_check(ast_t* ast) {
+	bool out = true;
+
+	for (usize i = 0; i < ast->types.size; i++) {
+		out &= ast_check_type_decl(ast, &ast->types.data[i]);
+	}
+
+	return out;
+}
+
 int compass_main() {
 	arena_t arena = arena_new();
 	u8_array_t file = file_read(&arena, str_lit(&arena, "test.tog"));
@@ -26,6 +77,7 @@ int compass_main() {
 
 	ast_t ast = ast_parse(&arena, &tokens);
 	ast_print(&ast);
+	ast_check(&ast);
 
 	arena_destroy(&arena);
 	return 0;
